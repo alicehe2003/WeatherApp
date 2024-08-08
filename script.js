4537bc479c80b7bdab96016c2126d064787f5a8c
alicehe2003 <140996846+alicehe2003@users.noreply.github.com>
2024-08-08 16:27:09 -0700

    Retrieved information regarding weather and location into local variables in js.

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ import { config } from "./config.js";
 const key = config.API_KEY; 
 let call = `http://api.weatherapi.com/v1/current.json?key=${key}&q=Vancouver&aqi=no`; 
 
-fetch(call, {mode: 'cors'}) 
-.then(function(response) {
-    return response.json(); 
-})
-.then(function(response) {
-    console.log(response); 
-})
-.catch(function(error) {
-    console.error("Error: ", error); 
-}); 
+let location = ""; 
+let region = ""; 
+let country = ""; 
+let weather = ""; 
+
+function getInformation() {
+    fetch(call, {mode: 'cors'}) 
+    .then(function(response) {
+        return response.json(); 
+    })
+    .then(function(response) { 
+        location = response.location.name; 
+        region = response.location.region; 
+        country = response.location.country; 
+        weather = response.current.condition.text; 
+
+        console.log("Location:", location); 
+        console.log("Region:", region); 
+        console.log("Country:", country); 
+        console.log("Weather:", weather); 
+    })
+    .catch(function(error) {
+        console.error("Error: ", error); 
+    }); 
+}
+
