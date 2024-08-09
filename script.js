3df9139af656a9b9d7f69ae3697620d6ba169f71
alicehe2003 <140996846+alicehe2003@users.noreply.github.com>
2024-08-08 17:24:43 -0700

    Implemented html and js to retrieve information given a city.

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 import { config } from "./config.js"; 
 
 const key = config.API_KEY; 
-let call = `http://api.weatherapi.com/v1/current.json?key=${key}&q=Vancouver&aqi=no`; 
 
 let location = ""; 
 let region = ""; 
 let country = ""; 
 let weather = ""; 
+let feelslike = ""; 
 
-function getInformation() {
+export function getInformation(event) {
+    event.preventDefault(); 
+
+    // get city from input 
+    const city = document.getElementById("city").value; 
+    const call = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`; 
+
+
     fetch(call, {mode: 'cors'}) 
     .then(function(response) {
         return response.json(); 
@@ -18,14 +25,18 @@ function getInformation() {
         region = response.location.region; 
         country = response.location.country; 
         weather = response.current.condition.text; 
+        feelslike = response.current.feelslike_c; 
 
         console.log("Location:", location); 
         console.log("Region:", region); 
         console.log("Country:", country); 
         console.log("Weather:", weather); 
+        console.log("Feels like:", feelslike); 
     })
     .catch(function(error) {
         console.error("Error: ", error); 
     }); 
 }
+
+window.getInformation = getInformation; 
 
